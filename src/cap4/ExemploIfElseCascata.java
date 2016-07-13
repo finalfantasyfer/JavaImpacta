8fa51a44472cc4904011f786edb99eacdb1bd131
finalfantasyfer <fernando.nsc@gmail.com>
2016-07-13 09:09:32 -0300

    Add files via upload

---
+++ src/cap4/ExemploIfElseCascata.java
@@ -0,0 +1,14 @@
+package cap4;
+
+public class ExemploIfElseCascata {
+	public static void main(String[] args) {
+		int hora = 11;
+		if (hora < 12) {
+			System.out.println("Bom dia!");
+		} else if (hora < 18) {
+			System.out.println("Boa Tarde!");
+		} else {
+			System.out.println("Boa Noite!");
+		}
+	}
+}
