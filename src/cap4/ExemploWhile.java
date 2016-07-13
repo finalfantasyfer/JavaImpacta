03f0d291bfb0f4c8ee2209d6d73ad34babcee3ae
finalfantasyfer <fernando.nsc@gmail.com>
2016-07-13 09:54:22 -0300

    2016.07.13

---
+++ src/cap4/ExemploWhile.java
@@ -0,0 +1,11 @@
+package cap4;
+
+public class ExemploWhile {
+	public static void main(String args[]) {
+		int num = 10;
+		while (num < 1500) {
+			num *= 10;
+			System.out.println("Valor de num é " + num);
+		}
+	}
+}
